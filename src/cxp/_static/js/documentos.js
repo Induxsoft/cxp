7a482f00f259d9ad7233f3ee7f752ec21699efae
Francisco Ávila <50455756+7oni3c@users.noreply.github.com>
2023-12-28 18:29:29 -0600

    update

--- src/cxp/_static/js/documentos.js
+++ src/cxp/_static/js/documentos.js
@@ -1,4 +1,4 @@
-var proveedor =
+var documento =
 {
     tableId: "", table: null,
 
@@ -30,64 +30,21 @@ var proveedor =
         if (this.table.CurrentRowIndex() < 0) { alert("Debe seleccionar una fila"); return; }
 
         var data = this.table.DataArray[this.table.CurrentRowIndex()];
-        window.location.href = url.replace("@proveedor",data.sys_pk);
+        window.location.href = url.replace("@doc",data.sys_pk);
     },
 
-    list: {
-        tbl_proveedores: null,
-        txt_search_proveedor: null,
-        btn_search_proveedor: null,
-        btn_new_proveedor: null,
+    list: {},
 
-        init()
-        {
-            this.txt_search_proveedor = document.getElementById("txt_search_proveedor");
-            this.btn_search_proveedor = document.getElementById("btn_search_proveedor");
-            this.btn_new_proveedor = document.getElementById("btn_new_proveedor");
-            this.tbl_proveedores = document.getElementById("tbl_proveedores");
+    form: {},
 
-            if (this.txt_search_proveedor) {
-                this.txt_search_proveedor.addEventListener("keydown", (e) => {
-                    if (e.key === "Enter") this.buscarProveedor();
-                });
-            }
-            if (this.btn_search_proveedor) {
-                this.btn_search_proveedor.addEventListener("click", () => { this.buscarProveedor(); });
-            }
-        },
-
-        buscarProveedor() {
-            let text = this.txt_search_proveedor.value.trim();
-            let url = this.txt_search_proveedor.getAttribute("data-url-search").trim();
-            if (!text) return;
-            if (!url) { alert("No se indico un destino"); return; }
-            if (!this.tbl_proveedores) { alert("No se ha definido la tabla de proveedores."); return; }
-            url = url.replace("@search",text);
-            
-            let onSuccess = (data) => {
-                if (data.message) { alert(data.message); }
-    
-                this.tbl_proveedores.DataArray = data;
-                this.tbl_proveedores._printRows();
-            }
-            let onFailure = (error) => {
-                alert('No se pudo realizar la busqueda.\n' + JSON.stringify(error));
-            }
-    
-            InduxsoftCrudlModel.InvokeService(url,null,onSuccess,onFailure,"GET",false);
-        }
-    },
-
-    form: {
-        formProveedor: null,
-        fcElements: null,
+    crear: {
+        formcxp: null,
+        elements: null,
         btnSave: null,
-        url_buscar_edoprov: "",
-        url_buscar_ciudad: "",
-        
+
         init()
         {
-            this.formProveedor = document.getElementById("form_proveedor");
+            this.formcxp = document.getElementById("form_cxp");
             this.btnSave = document.getElementById("btn_save");
             this.setEvents();
         },
@@ -95,122 +52,36 @@ var proveedor =
         setEvents()
         {
             if (this.btnSave) { this.btnSave.addEventListener("click", () => { this.saveForm(); }); }
-            if (this.formProveedor) {
-                this.fcElements = this.formProveedor.elements;
+            if (this.formcxp) {
+                this.elements = this.formcxp.elements;
 
-                this.fcElements["chq_domicilio1"].addEventListener("change", (event) => {
-                    let domicilio1 = document.getElementById("cbody_domicilio1");
-                    (event.target.checked) ? domicilio1.classList.remove("disable-form") : domicilio1.classList.add("disable-form");
-                    if (this.fcElements["sel_estado"].options.length <= 0) this.fillEstados(this.fcElements["sel_pais"],this.fcElements["sel_estado"]);
-                });
-                this.fcElements["sel_pais"].addEventListener("change", () => {
-                    this.fillEstados(this.fcElements["sel_pais"],this.fcElements["sel_estado"]);
-                });
-                this.fcElements["sel_estado"].addEventListener("change", () => {
-                    this.fillCiudades(this.fcElements["sel_estado"],this.fcElements["sel_ciudad"]);
-                });
-                
-                this.fcElements["chq_domicilio2"].addEventListener("change", (event) => {
-                    let domicilio2 = document.getElementById("cbody_domicilio2");
-                    (event.target.checked) ? domicilio2.classList.remove("disable-form") : domicilio2.classList.add("disable-form");
-                    if (this.fcElements["sel_estado2"].options.length <= 0) this.fillEstados(this.fcElements["sel_pais2"],this.fcElements["sel_estado2"]);
-                });
-                this.fcElements["sel_pais2"].addEventListener("change", () => {
-                    this.fillEstados(this.fcElements["sel_pais2"],this.fcElements["sel_estado2"]);
-                });
-                this.fcElements["sel_estado2"].addEventListener("change", () => {
-                    this.fillCiudades(this.fcElements["sel_estado2"],this.fcElements["sel_ciudad2"]);
-                });
+                let ik_proveedor = document.getElementById("sel_proveedor");
+                ik_proveedor.addEventListener("change",function(data) {
+                    let txt_divisa = document.getElementById("txt_divisa");
+                    let txt_tcambio = document.getElementById("txt_tcambio");
 
-                this.fcElements["chq_domicilio3"].addEventListener("change", (event) => {
-                    let domicilio3 = document.getElementById("cbody_domicilio3");
-                    (event.target.checked) ? domicilio3.classList.remove("disable-form") : domicilio3.classList.add("disable-form");
-                    if (this.fcElements["sel_estado3"].options.length <= 0) this.fillEstados(this.fcElements["sel_pais3"],this.fcElements["sel_estado3"]);
-                });
-                this.fcElements["sel_pais3"].addEventListener("change", () => {
-                    this.fillEstados(this.fcElements["sel_pais3"],this.fcElements["sel_estado3"]);
-                });
-                this.fcElements["sel_estado3"].addEventListener("change", () => {
-                    this.fillCiudades(this.fcElements["sel_estado3"],this.fcElements["sel_ciudad3"]);
-                });
-
-                this.fcElements["chq_otorgar_credito"].addEventListener("change", (event) => {
-                    let div_credito = document.getElementById("div_otorgar_credito");
-                    (event.target.checked) ? div_credito.classList.remove("disable-form") : div_credito.classList.add("disable-form");
-                });
-                this.fcElements["rd_credito_ilimitado"].addEventListener("change", (event) => {
-                    this.fcElements["limitecredito"].type = "hidden";
-                });
-                this.fcElements["rd_credito_limitado"].addEventListener("change", (event) => {
-                    this.fcElements["limitecredito"].type = "number";
+                    txt_divisa.value = data.divisa;
+                    txt_tcambio.value = data.tcambio;
                 });
             }
-        },
-
-        fillEstados(ref,out){
-            let url = this.url_buscar_edoprov.replace("search","ipais");
-            url = InduxsoftCrudlModel.UrlReplace(url,{ipais:ref.value});
-
-            let onSuccess = (data) => {
-                if (data.message) {
-                    console.error(data.message);
-                    return;
-                }
-
-                out.innerHTML = "";
-                data.forEach(item => {
-                    const option = document.createElement("option");
-                    option.value = item.sys_pk;
-                    option.text = item.text;
-
-                    out.appendChild(option);
-                });
-                proveedor.trigger(out,"change");
-            }
-            let onFailure = (error) => { console.error(error) }
-            InduxsoftCrudlModel.InvokeService(url,null,onSuccess,onFailure,"GET",false,false);
-        },
-
-        fillCiudades(ref,out){
-            let url = this.url_buscar_ciudad.replace("search","iestado");
-            url = InduxsoftCrudlModel.UrlReplace(url,{iestado:ref.value});
-
-            let onSuccess = (data) => {
-                if (data.message) {
-                    console.error(data.message);
-                    return;
-                }
-
-                out.innerHTML = "";
-                data.forEach(item => {
-                    const option = document.createElement("option");
-                    option.value = item.sys_pk;
-                    option.text = item.text;
-
-                    out.appendChild(option);
-                });
-            }
-            let onFailure = (error) => { console.error(error) }
-            InduxsoftCrudlModel.InvokeService(url,null,onSuccess,onFailure,"GET",false,false);
         },
 
         saveForm(){
-            if (!this.formProveedor.reportValidity()) return;
-
-            this.formProveedor.submit();
+            if (!this.formcxp.reportValidity()) return;
+            this.formcxp.submit();
         },
     },
 
     pago: {
-        formPagoProv: null,
+        formPagoCXP: null,
         elements: null,
         btnSave: null,
-        dtProv: {},
+        dtCXP: {},
         dvsPred: {},
 
         init()
         {
-            this.formPagoProv = document.getElementById("form_pagoprov");
+            this.formPagoCXP = document.getElementById("form_pagocxp");
             this.btnSave = document.getElementById("btn_save");
             this.setEvents();
         },
@@ -218,8 +89,15 @@ var proveedor =
         setEvents()
         {
             if (this.btnSave) { this.btnSave.addEventListener("click", () => { this.saveForm(); }); }
-            if (this.formPagoProv) {
-                this.elements = this.formPagoProv.elements;
+            if (this.formPagoCXP) {
+                this.elements = this.formPagoCXP.elements;
+
+                this.elements["txt_importe"].addEventListener("input", (event) => {
+                    let importe = Number(event.target.value);
+                    let saldo = Number(this.dtCXP.saldo);
+
+                    this.elements["txt_nuevo_saldo"].value = Math.sub(saldo,importe);
+                });
 
                 this.elements["sel_cuenta_retiro"].addEventListener("change", (event) => {
                     let option = event.target.options[event.target.selectedIndex];
@@ -229,7 +107,7 @@ var proveedor =
                     this.pedirTCambio();
 
                     this.elements["txt_tcambio_retiro"].value = cambio;
-                    proveedor.trigger(this.elements["txt_tcambio_retiro"],"change");
+                    documento.trigger(this.elements["txt_tcambio_retiro"],"change");
                 });
                 
                 this.elements["txt_tcambio_retiro"].addEventListener("change", (event) => {
@@ -251,6 +129,7 @@ var proveedor =
                     importe_pdr = Math.div(importe_pdr,tcambio_pdr);
 
                     this.elements["txt_importe"].value = importe_pdr;
+                    documento.trigger(this.elements["txt_importe"],"input");
                 });
 
                 this.elements["txt_importe"].addEventListener("change", (event) => {
@@ -269,7 +148,7 @@ var proveedor =
         pedirTCambio(){
             let optCtaR = this.elements["sel_cuenta_retiro"].options[this.elements["sel_cuenta_retiro"].selectedIndex];
             let cDvsPred = (this.dvsPred.codigo).toUpperCase();
-            let cDvsProv = (this.dtProv.divisa).toUpperCase();
+            let cDvsProv = (this.dtCXP.divisa).toUpperCase();
             let cDvsCtaR = optCtaR.getAttribute("data-divisa").toUpperCase();
 
             let hide_tcambio_ret = false;
@@ -351,20 +230,21 @@ var proveedor =
         },
 
         saveForm(){
-            if (!this.formPagoProv.reportValidity()) return;
-
-            this.formPagoProv.submit();
+            if (!this.formPagoCXP.reportValidity()) return;
+            this.formPagoCXP.submit();
         },
     },
 
     bonificacion: {
-        formBonificacion: null,
+        formBonificacionCXP: null,
         elements: null,
         btnSave: null,
+        dtCXP: {},
+        dvsPred: {},
 
         init()
         {
-            this.formBonificacion = document.getElementById("form_bonificacion_prov");
+            this.formBonificacionCXP = document.getElementById("form_bonificacion_cxp");
             this.btnSave = document.getElementById("btn_save");
             this.setEvents();
         },
@@ -372,13 +252,56 @@ var proveedor =
         setEvents()
         {
             if (this.btnSave) { this.btnSave.addEventListener("click", () => { this.saveForm(); }); }
-            if (this.formBonificacion) { this.elements = this.formBonificacion.elements; }
+            if (this.formBonificacionCXP) {
+                this.elements = this.formBonificacionCXP.elements;
+
+                this.elements["txt_importe"].addEventListener("input", (event) => {
+                    let importe = Number(event.target.value);
+                    let saldo = Number(this.dtCXP.saldo);
+
+                    this.elements["txt_nuevo_saldo"].value = Math.sub(saldo,importe);
+                });
+            }
         },
 
         saveForm(){
-            if (!this.formBonificacion.reportValidity()) return;
+            if (!this.formBonificacionCXP.reportValidity()) return;
+            this.formBonificacionCXP.submit();
+        },
+    },
 
-            this.formBonificacion.submit();
+    intmor: {
+        formIntMor: null,
+        elements: null,
+        btnSave: null,
+        dtCXP: {},
+        dvsPred: {},
+
+        init()
+        {
+            this.formIntMor = document.getElementById("form_intmor");
+            this.btnSave = document.getElementById("btn_save");
+            this.setEvents();
+        },
+
+        setEvents()
+        {
+            if (this.btnSave) { this.btnSave.addEventListener("click", () => { this.saveForm(); }); }
+            if (this.formIntMor) {
+                this.elements = this.formIntMor.elements;
+
+                this.elements["txt_importe"].addEventListener("input", (event) => {
+                    let importe = Number(event.target.value);
+                    let saldo = Number(this.dtCXP.saldo);
+
+                    this.elements["txt_nuevo_saldo"].value = Math.add(saldo,importe);
+                });
+            }
+        },
+
+        saveForm(){
+            if (!this.formIntMor.reportValidity()) return;
+            this.formIntMor.submit();
         },
     },
 }
