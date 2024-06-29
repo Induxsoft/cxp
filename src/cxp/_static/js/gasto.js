74acd1020af9a25199be8ae0309f6e1a6135271b
Candelario <candelario.hernandez@induxsoft.net>
2024-06-29 11:03:29 -0600

    Se agregó la opción de adjuntar documentos

---
+++ src/cxp/_static/js/gasto.js
@@ -0,0 +1,70 @@
+
+
+
+var gasto=
+{
+    form:
+    {
+        init()
+        {
+            this.txt_fecha=document.getElementById("txt_fecha");
+            this.txt_referencia=document.getElementById("txt_referencia");
+            this.file=document.getElementById("file");
+
+            if(this.file)this.file.addEventListener("change",()=>{gasto.form.uploadFile();});
+
+            this.media_list=document.getElementById("media-list");
+            if(this.media_list)this.media_list.onClicking=data=>
+            {
+                this.SelectedElement(data);
+            }
+        },
+        uploadFile()
+        {
+            if(this.file.value.trim()=="")return;
+            if(this.file.files.length<1)return;
+            
+            var data=new FormData();
+            for (let i = 0; i < this.file.files.length; i++) 
+            {
+                var file = this.file.files[i];
+                data.append(file.name,file);    
+            }
+            data.append("onlyfile",true);
+
+            InduxsoftCrudlModel.InvokeService(".",data,
+            (result)=>
+            {
+                this.file.value="";
+                window.location.reload();
+            },
+            (error)=>
+            {
+                this.file.value="";
+                alert(error.message??JSON.stringify(error));
+            },"PUT",false,true,"",true);
+        },
+        data_preview:null,
+        SelectedElement(data)
+        {
+            this.data_preview=this.getDataById(data.__internal_id__);
+        },
+        preview()
+        {
+            var data=this.data_preview;
+            if(!data)
+            {
+                alert("Debe seleccionar un elemento");
+                return;
+            }
+            
+            window.open(data.url,"_blank");
+        },
+        getDataById(id)
+        {
+            var data= this.media_list.getData(false).find(e=>e.__internal_id__==id);
+            data["index"]= this.media_list.getData(false).findIndex(e=>e.__internal_id__==id);
+            return data;
+        },
+    }
+}
